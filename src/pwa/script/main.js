9c98c7cbcc29d98e9842a92bd722c219661fd8e1
gullocean <gullocean@gmail.com>
2018-11-23 14:17:09 -0400

    update the path of serviceworker

--- src/pwa/script/main.js
+++ src/pwa/script/main.js
@@ -3,7 +3,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./serviceworker.js')
+             .register('./../serviceworker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 });
