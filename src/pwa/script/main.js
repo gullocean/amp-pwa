ed05f82a38f5a0d56272a2dbbc636bd8479f0fa5
gullocean <gullocean@gmail.com>
2018-11-23 16:19:37 -0400

    add checking mobile device

--- src/pwa/script/main.js
+++ src/pwa/script/main.js
@@ -3,3 +3,23 @@ if ('serviceWorker' in navigator) {
            .register('./serviceworker.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
+
+function detectMobile() {
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+if (!detectMobile()) {
+  document.body.innerHTML = '<h1>Please use your mobile phone</h1>';
+}
