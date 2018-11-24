964697f5defee8078b21a2cc0aa538d436211828
gullocean <gullocean@gmail.com>
2018-11-23 23:23:56 -0400

    update

--- src/pwa/serviceworker.js
+++ src/pwa/serviceworker.js
@@ -1,20 +1,20 @@
-var CACHE_NAME = 'gullocean-amp-pwa-cache-v1';
-var urlsToCache = [
-  '/pwa',
-  '/pwa/styles/main.css',
-  '/pwa/script/main.js'
-];
+// var CACHE_NAME = 'gullocean-amp-pwa-cache-v1';
+// var urlsToCache = [
+//   '/pwa',
+//   '/pwa/styles/main.css',
+//   '/pwa/script/main.js'
+// ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
 // self.addEventListener('fetch', event => {
 //   if (event.request.mode === 'navigate') {
