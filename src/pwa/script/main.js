5702ab050404c76dc92ad3b4b1ee28ee1cac7d13
gullocean <gullocean@gmail.com>
2018-11-23 23:10:33 -0400

    update

--- src/pwa/script/main.js
+++ src/pwa/script/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('./serviceworker.js')
+           .register('serviceworker.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
